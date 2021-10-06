f5802e4f9670fa7aadc39a1d22cb799c4100faa2
mostafa <hitmanx97@gmail.com>
2021-10-06 14:49:48 +0200

    fix bugs

--- lib/shared/components/card.dart
+++ lib/shared/components/card.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:spy_game_responsive/shared/components/card_side.dart';
 
-Widget card(Size size, {required String image, String text = ''}) {
-  return Material(
-    elevation: 6,
-    borderRadius: BorderRadius.circular(15),
-    clipBehavior: Clip.antiAliasWithSaveLayer,
-    color: Colors.transparent,
-    child: Container(
-      alignment: Alignment.center,
-      height: size.height,
-      width: size.height * .5,
-      child: Text(text),
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('images/$image.PNG'), fit: BoxFit.fill)),
+Widget card(Size size, {required int cardNumber, String location = ''}) {
+  return FlipCard(
+    direction: FlipDirection.HORIZONTAL, // default
+    front: cardSide(
+      size,
+      image: 'card',
+      text: cardNumber.toString(),
+    ),
+    back: cardSide(
+      size,
+      image: location == '' ? 'spy' : 'recard',
+      text: location == '' ? '' : location,
     ),
   );
 }
